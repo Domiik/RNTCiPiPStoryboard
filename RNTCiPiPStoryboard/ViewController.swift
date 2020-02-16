c5876f144a8a712e00068d507ba5bbe21ac4ad07
Domiik <memfis15@yandex.ru>
2020-02-16 15:56:42 +0300

    Initial Commit

---
+++ RNTCiPiPStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNTCiPiPStoryboard
+//
+//  Created by Domiik on 16.02.2020.
+//  Copyright © 2020 Domiik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
